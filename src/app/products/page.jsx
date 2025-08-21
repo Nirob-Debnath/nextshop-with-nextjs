1727b11015ca64cb174dbb976d99b1775f90d25d
unknown <nirobdebnath456@gmail.com>
2025-08-21 15:32:13 +0600

    sign up & login page done

---
+++ src/app/products/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1>Products</h1>
+            <p>Explore our wide range of products.</p>
+        </div>
+    );
+};
+
+export default page;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,61 @@
-import Link from 'next/link';
-import React from 'react';
+"use client";
+import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
+import React from "react";
 
 const Navbar = () => {
+    const { data: session } = useSession();
 
-    const links = <>
-        <li><Link href="/">Item 1</Link></li>
-        <li><Link href="/">Item 2</Link></li>
-        <li><Link href="/">Item 3</Link></li>
-    </>
+    const links = (
+        <>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/products">Products</Link></li>
+            {session && <li><Link href="/dashboard/add-product">Add Product</Link></li>}
+        </>
+    );
 
     return (
         <div className="navbar bg-base-100 shadow-sm">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
                     </div>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
+                    >
                         {links}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">Next Shop</a>
+                <Link href="/" className="btn btn-ghost text-xl">NextShop</Link>
             </div>
+
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {links}
-                </ul>
+                <ul className="menu menu-horizontal px-1">{links}</ul>
             </div>
+
             <div className="navbar-end">
-                <a className="btn">Login</a>
+                {session ? (
+                    <button
+                        onClick={() => signOut()}
+                        className="btn bg-red-500 text-white hover:bg-red-600"
+                    >
+                        Logout
+                    </button>
+                ) : (
+                    <Link href="/login" className="btn bg-blue-500 text-white hover:bg-blue-600">
+                        Login
+                    </Link>
+                )}
             </div>
         </div>
     );
